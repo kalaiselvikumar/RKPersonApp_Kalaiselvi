082ee15cde48c3b8a0886b5da847a66d5eb50b2a
Chris Evans <scev@live.com>
2018-04-05 17:46:46 -0500

    Added

---
+++ PersonApplication.DataAccess/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication.DataAccess
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string Email { get; set; }
+    }
+}
